475acbbca7c46e6be503cfee8b919153f48f444e
hymced <hymced@gmail.com>
2023-06-07 13:18:38 +0200

    client-side + server-side validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,8 @@ const bcryptjs = require("bcryptjs");
 
 const router = express.Router();
 
-const UserModel = require("../models/User.model")
+const UserModel = require("../models/User.model");
+const { default: mongoose } = require('mongoose');
 
 const saltRounds = 12; // default is 10
 
@@ -15,22 +16,32 @@ router.get("/signup", (req, res, next) => {
 // POST /signup
 router.post("/signup", (req, res, next) => {
     const {email, password} = req.body;
+    if (!email || !password) {
+        res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your email and password.' });
+        return; 
+    }
     bcryptjs
-		// .genSalt() // uses default
+        // .genSalt() // uses default
         .genSalt(saltRounds)
         .then(salt => {
-			return bcryptjs.hash(password, salt);
+            return bcryptjs.hash(password, salt);
         })
         .then(hash => {
             // const {email} = req.body.email
-			// return UserModel.create({email, passwordHash: hash})
-			return UserModel.create({email: req.body.email, passwordHash: hash})
+            // return UserModel.create({email, passwordHash: hash})
+            // return UserModel.create({email: req.body.email, passwordHash: hash})
+            return UserModel.create({email: req.body.email}) // generate a mongoose.Error.ValidationError 
+            // (one can also copy as fetch the request from chrome dev tools > network, tamper the data, then paste in console)
         })
-		.then(user => {
-			res.redirect("/user-profile")
-		})
+        .then(user => {
+            res.redirect("/user-profile")
+        })
         .catch(e => {
-            next(e)
+            if (e instanceof mongoose.Error.ValidationError) {
+                res.status(400).render('auth/signup', { errorMessage: e.message });
+            } else {
+                next(e)
+            }
         });
 })
 
